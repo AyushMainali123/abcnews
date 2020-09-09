aadfd94e322eead3a509e298bc306323f5a0a3cf
Ayush <49358949+AyushMainali123@users.noreply.github.com>
2020-09-09 14:34:23 +0545

    Updated HTML, CSS, JS

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ const playButtonSelectors = document.querySelectorAll(".fa-play");
 const youtubeVideoSelector = document.querySelector("#youtubeVideo");
 const iframeSelector = document.querySelector("#iframeVideo");
 
-
-
-
-
 playButtonSelectors.forEach((selector) => {
   selector.addEventListener("click", () => {
     iframeSelector.src = selector?.dataset?.link
@@ -22,9 +18,22 @@ const numberOfSlides = () => {
 var mySwiper = new Swiper(".swiper-container", {
   // Optional parameters
   loop: true,
-  slidesPerView: numberOfSlides(),
-  slidesPerColumn: 1,
+  slidesPerView: 1,
   spaceBetween: 10,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1250: {
+      slidesPerView: 3,
+    },
+    1664: {
+      slidesPerView: 4,
+    },
+    2500: {
+      slidesPerView: 5,
+    }
+  },
 
   // If we need pagination
   pagination: {
@@ -41,6 +50,46 @@ var mySwiper = new Swiper(".swiper-container", {
   scrollbar: {
     el: ".swiper-scrollbar",
   },
+  debugger: true
+});
+
+
+var slugSwiper = new Swiper(".swiper-container-slug", {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    500: {
+      slidesPerView: 2,
+    },
+    1000: {
+      slidesPerView: 3,
+    },
+    1500: {
+      slidesPerView: 4,
+    },
+    2000: {
+      slidesPerView: 5,
+    }
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  debugger: true
 });
 
 // Spinner
@@ -54,3 +103,31 @@ document.onreadystatechange = function () {
     document.querySelector("body").style.visibility = "visible";
   }
 };
+
+
+// Carousel Container
+
+const Swiper1 = new Swiper(".swiper-container1", {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+  slidesPerColumn: 1,
+  spaceBetween: 10,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination2",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next2",
+    prevEl: ".swiper-button-prev2",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".carousel-scrollbar2",
+  },
+  debugger: true
+});
